d166be03010a8370a20ed9fea6d4326be28a20b2
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-12 15:30:05 -0600

    Add changes to belong_to assertion in pet_application_spec file from plural belongs to singular

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe PetApplication, type: :model do
   describe "relationships" do
-    it { should belongs_to(:pet)}
-    it { should belongs_to(:application)}
+    it { should belong_to(:pet)}
+    it { should belong_to(:application)}
   end
 end
