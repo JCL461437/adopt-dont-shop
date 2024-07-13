4bba26f1bf985ac054f0a01d8ac6f5b006082136
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-12 20:23:49 -0600

    Added create and new actions to applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,20 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    @application = Application.create(new_app)
+    
+    redirect_to "application/#{@application.id}"
+  end
+
+  private 
+  
+  def application_params 
+    params.permit(params[:name], params[:street_address], params[:city], params[:state], params[:zip_code], params[:description])
+  end
 end
