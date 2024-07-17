f234fea6f457d36ccde9a670192b554d1d784ca4
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-17 16:55:19 -0600

    Add model method for gathering all pet_applications that correspond to a particular applicaiton id

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,5 +2,9 @@ class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
 
-  validates :pet_id, application_id:, presence: true
+  validates :pet_id, :application_id, presence: true
+
+  def self.associated_applications(application)
+    where(:application_id == application)
+  end
 end
