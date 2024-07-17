acf408aaa809308c8d514be9dca34f007455fbdd
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-17 10:55:40 -0600

    Add depedent: :destroy for all associated models

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -5,9 +5,20 @@ RSpec.describe "the admin shelters index" do
     @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     @shelter_2 = Shelter.create(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
     @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
-    @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
-    @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
-    @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
+    @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
+    @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
+    @pet_3 = @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
+    
+    @application1 = Application.create!(name: "Joe Baeza", street_address: "12345 Joe Street", city: "Irvine", state: "CA", zip_code: "54321",
+                                        description: "I like Clawdia so I want to adopt them.", status: "In Progress")
+    @application2 = Application.create!(name: "Billy Bob", street_address: "56789 The Bob Street", city: "Boulder", state: "CO", zip_code: "09876",
+                                        description: "I'll go for Mr. Pirate since that name sounds sick.", status: "Pending")
+    @application3 = Application.create!(name: "The Guy", street_address: "111111 The Guy Avenue", city: "Seattle", state: "WA", zip_code: "29212",
+                                        description: "Lucille Bald looks like a sphynx and I think I am a sphynx kind of guy.", status: "Pending")
+
+    @pet_application1 = PetApplication.create!(application: @application1, pet: @pet_2)
+    @pet_application2 = PetApplication.create!(application: @application2, pet: @pet_1)
+    @pet_application3 = PetApplication.create!(application: @application3, pet: @pet_3)
   end
 
   it "lists all the shelter names in reverse alphabetical order" do
@@ -106,5 +117,19 @@ RSpec.describe "the admin shelters index" do
 
     expect(page).to have_content(@shelter_2.name)
     expect(page).to_not have_content(@shelter_1.name)
+  end
+
+  describe "displays a section for shelters with pending applications" do
+    it "in this section I see the name of every shelter that has a pending application" do
+      visit "/admin/shelters"
+      
+      expect(page).to have_content("Shelters with Pending Applications")
+
+      within "#shelters-pending-applicaiton" do
+        expect(page).to have_content("#{pet_application2}")
+        expect(page).to have_content("#{pet_application3}")
+        expect(page).to_not have_content("#{pet_application1}")
+      end
+    end
   end
 end
