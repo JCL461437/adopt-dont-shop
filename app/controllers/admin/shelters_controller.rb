62fd011b587910dafcba270fe0ba146568b65787
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-17 09:37:05 -0600

    Merge the complete us 9 branch Kaelin completed

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,3 +1,11 @@
 class SheltersController < ApplicationController
-  
+  def index
+    if params[:sort].present? && params[:sort] == "pet_count"
+      @shelters = Shelter.order_by_number_of_pets
+    elsif params[:search].present?
+      @shelters = Shelter.search(params[:search])
+    else
+      @shelters = Shelter.order_by_recently_created
+    end
+  end
 end
