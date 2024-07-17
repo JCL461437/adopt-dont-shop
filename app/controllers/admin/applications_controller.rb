d7b983762a1db341d4ba6fbe7a3c3beaee2639dd
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-17 16:54:31 -0600

    Added create and update actions to the admin/applications controller

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,7 +1,16 @@
 class Admin::ApplicationsController < ApplicationsController
   def show
     @application = Application.find(params[:id])
+    @pet_applications = PetApplication.associated_applications(@application.id)
+    @pets = Pet.find_pet_by_name(params[:name.downcase]) if params[:name].present?
   end
 
-  
+  def update 
+    binding.pry
+    @application = Application.find(params[:id])
+    @pet_application = @application.pet_applications.find_by(params[:pet_id])
+    if @pet_application.update(pet_app_status: params[:pet_app_status])
+      redirect_to "/admin/applications/#{@application.id}"
+    end
+  end
 end
